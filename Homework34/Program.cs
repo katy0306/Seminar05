2fc4f724d2c1daf2399361645ac17abfa271a561
katy0306 <Sarafanchuk@mail.ru>
2022-08-26 15:03:17 +0300

    Homework34

--- Homework34/Program.cs
+++ Homework34/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Массив заполненный случайными положительными трёхзначными числами:");
+
+int[] MyArray = new int[6];
+int Index = 0;
+int CountEven = 0;
+
+while (Index < MyArray.Length)
+{
+    MyArray[Index] = new Random().Next(100, 500);  
+    if (MyArray[Index] % 2 ==0) 
+    {
+        CountEven++;
+    }
+    Console.Write($"{MyArray[Index]}, ");
+    Index++; 
+}
+Console.WriteLine("\n");
+Console.WriteLine($"Колличество четных чисел в данном массиве: {CountEven}");
